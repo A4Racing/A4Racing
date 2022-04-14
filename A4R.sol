ccbe89f933bde1906cede1f6e0721b0f66040ff4
A4R <103581736+a4rtoken@users.noreply.github.com>
2022-04-14 11:26:28 +0530

    contract added

---
+++ A4R.sol
@@ -0,0 +1,30 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+import "@openzeppelin/contracts/security/Pausable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20FlashMint.sol";
+
+contract A4RacingCoin is ERC20, ERC20Burnable, Pausable, Ownable, ERC20FlashMint {
+    constructor() ERC20("A4Racing", "A4R") {
+        _mint(msg.sender, 100000000000 * 10 ** decimals());
+    }
+
+    function pause() public onlyOwner {
+        _pause();
+    }
+
+    function unpause() public onlyOwner {
+        _unpause();
+    }
+
+    function _beforeTokenTransfer(address from, address to, uint256 amount)
+        internal
+        whenNotPaused
+        override
+    {
+        super._beforeTokenTransfer(from, to, amount);
+    }
+}
